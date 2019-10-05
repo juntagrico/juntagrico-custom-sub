9dcb248e98f9fa35ecac1658db935dea6b035579
flurin-conradin <f.conradin@gmail.com>
2019-10-05 18:52:27 +0200

    add constraint

--- juntagrico_custom_sub/entity/subscription_size_mandatory_products.py
+++ juntagrico_custom_sub/entity/subscription_size_mandatory_products.py
@@ -2,7 +2,11 @@ from django.db import models
 from juntagrico.models import SubscriptionSize
 from juntagrico_custom_sub.entity.product import Product
 
+
 class SubscriptionSizeMandatoryProducts(models.Model):
-    subscription_size = models.ForeignKey(SubscriptionSize,on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    subscription_size = models.ForeignKey(SubscriptionSize, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.IntegerField()
+    
+    class Meta:
+        unique_together = ("subscription_size", "product")
