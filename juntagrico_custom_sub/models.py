b5ae239195f146a01e07eee5834d383ca5094c58
flurin-conradin <f.conradin@gmail.com>
2019-10-11 16:57:04 +0200

    reformat imports
    avoids making juntagrico models importable through custom sub
    keeps namespace clean

--- juntagrico_custom_sub/models.py
+++ juntagrico_custom_sub/models.py
@@ -1,9 +1,15 @@
-from django.db import models
-
 from juntagrico_custom_sub.entity.product import *
-from juntagrico_custom_sub.entity.custom_delivery import *
-from juntagrico_custom_sub.entity.subscription_content import *
-from juntagrico_custom_sub.entity.subscription_content_item import *
-from juntagrico_custom_sub.entity.subscription_content_future_item import *
-from juntagrico_custom_sub.entity.subscription_size_mandatory_products import *
-
+from juntagrico_custom_sub.entity.custom_delivery import (
+    CustomDelivery, CustomDeliveryProduct
+)
+from juntagrico_custom_sub.entity.subscription_content import (
+    SubscriptionContent)
+from juntagrico_custom_sub.entity.subscription_content_item import (
+    SubscriptionContentItem
+)
+from juntagrico_custom_sub.entity.subscription_content_future_item import (
+    SubscriptionContentFutureItem
+)
+from juntagrico_custom_sub.entity.subscription_size_mandatory_products import (
+    SubscriptionSizeMandatoryProducts
+)
