d5c49e1aa553f009508a51cb4f1dc2c36e100525
flurin-conradin <f.conradin@gmail.com>
2019-11-15 10:03:34 +0100

    encure app installs as package
    only use local requirements in install_requires

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ SOURCE_DIR = os.path.join(ROOT_DIR)
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+
 def get_requirements(requirements_file):
     with open(requirements_file) as f:
         required = [line.split('#')[0] for line in f.read().splitlines()]
     return required
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='juntagrico-custom-sub',
@@ -35,9 +34,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=get_requirements(os.path.join(ROOT_DIR, 'requirements.txt')),
+    install_requires=get_requirements(os.path.join(ROOT_DIR, 'requirements-local.txt')),
 )
