4b25ca1785cb476cf32848d90f21cb7344842401
Kenny <kennyfloria@hotmail.com>
2020-01-21 22:27:09 +0100

    Fix staticfile load

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(requirements_file):
 
 setup(
     name='juntagrico-custom-sub',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
