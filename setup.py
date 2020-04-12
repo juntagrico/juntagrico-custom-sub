577264cf1dc71d2e6efe8a03f762f7be15d18702
Kenny <kennyfloria@hotmail.com>
2020-04-12 13:00:34 +0200

    Add sitenumbers to lists

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(requirements_file):
 
 setup(
     name='juntagrico-custom-sub',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     include_package_data=True,
     license='LPGLv3',  # example license
