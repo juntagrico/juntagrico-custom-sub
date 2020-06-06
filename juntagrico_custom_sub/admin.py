0a416bc3096ebe5d3d9b50b301d77a3e0826925a
flurin-conradin <f.conradin@gmail.com>
2020-06-06 18:27:22 +0200

    admin.py pep8

--- juntagrico_custom_sub/admin.py
+++ juntagrico_custom_sub/admin.py
@@ -1,33 +1,41 @@
 from django.contrib import admin
-from juntagrico.admins import BaseAdmin
 
-from juntagrico_custom_sub.models import *
+from juntagrico.admins import BaseAdmin
+from juntagrico_custom_sub import models as jcsm
+
 
 class SubItemsInline(admin.TabularInline):
-    model = SubscriptionContentItem
+    model = jcsm.SubscriptionContentItem
+
 
 class FutureSubItemsInline(admin.TabularInline):
-    model = SubscriptionContentFutureItem
+    model = jcsm.SubscriptionContentFutureItem
+
 
 class MandatoryProductInline(admin.TabularInline):
-    model = Product.mandatory_for_sizes.through
+    model = jcsm.Product.mandatory_for_sizes.through
+
 
 class SubscriptionContentAdmin(admin.ModelAdmin):
     inlines = [
-        SubItemsInline,FutureSubItemsInline
+        SubItemsInline, FutureSubItemsInline
     ]
 
-    
+
 class CustomDeliveryProductInline(admin.TabularInline):
-    model = CustomDeliveryProduct
+    model = jcsm.CustomDeliveryProduct
+
+
 class CustomDeliveryAdmin(admin.ModelAdmin):
     inlines = [
         CustomDeliveryProductInline
     ]
 
+
 class CustomDeliveryProductAdmin(BaseAdmin):
     list_display = ['name', 'code', 'units']
-    
-admin.site.register(Product,CustomDeliveryProductAdmin)
-admin.site.register(SubscriptionContent,SubscriptionContentAdmin)
-admin.site.register(CustomDelivery,CustomDeliveryAdmin)
+
+
+admin.site.register(jcsm.Product, CustomDeliveryProductAdmin)
+admin.site.register(jcsm.SubscriptionContent, SubscriptionContentAdmin)
+admin.site.register(jcsm.CustomDelivery, CustomDeliveryAdmin)
