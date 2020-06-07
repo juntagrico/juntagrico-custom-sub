a611a2876374e3faa8efbf0bf2b699fea0de8ce8
flurin-conradin <f.conradin@gmail.com>
2020-06-07 09:50:54 +0200

    improve admin for subscription content

--- juntagrico_custom_sub/admin.py
+++ juntagrico_custom_sub/admin.py
@@ -17,6 +17,33 @@ class MandatoryProductInline(admin.TabularInline):
 
 
 class SubscriptionContentAdmin(admin.ModelAdmin):
+    list_display = ('subscription_id', 'get_member', 'get_email', 'get_depot_name')
+
+    search_fields = [
+         'subscription__primary_member__email',
+         'subscription__primary_member__first_name',
+         'subscription__primary_member__last_name',
+         'subscription__depot__name']
+
+    def get_member(self, obj):
+        mem = obj.subscription.primary_member
+        return f"{mem.last_name}, {mem.first_name}"
+
+    get_member.short_description = 'Mitglied'
+    get_member.admin_order_field = 'subscription__primary_member__last_name'
+
+    def get_email(self, obj):
+        return obj.subscription.primary_member.email
+
+    get_email.short_description = 'E-Mail Mitglied'
+    get_email.admin_order_field = 'subscription__primary_member__email'
+
+    def get_depot_name(self, obj):
+        return obj.subscription.depot.name
+
+    get_depot_name.short_description = 'Depot'
+    get_depot_name.admin_order_field = 'subscription__depot__name'
+
     inlines = [
         SubItemsInline, FutureSubItemsInline
     ]
