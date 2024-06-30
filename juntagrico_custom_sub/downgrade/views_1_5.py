64323744fb072c69dd66b9438cd1219c89924cb2
pascalfree <pascal.free@ihdg.ch>
2024-07-01 01:39:23 +0200

    ruff

--- juntagrico_custom_sub/downgrade/views_1_5.py
+++ juntagrico_custom_sub/downgrade/views_1_5.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils import timezone
 
 from juntagrico.config import Config
 from juntagrico.dao.subscriptiontypedao import SubscriptionTypeDao
