35f8790bbd9dad994d1d85ff92a78971b098c97a
flurin-conradin <f.conradin@gmail.com>
2025-04-21 16:26:15 +0200

    remove reference to Subscription content
    in the content of custom sub this is now called custom
    https://github.com/juntagrico/juntagrico-custom-sub/pull/42/commits/49fe76ac997210293965d9a6cdf59a31349eddf1

--- juntagrico_custom_sub/entity/cs_depot.py
+++ juntagrico_custom_sub/entity/cs_depot.py
@@ -16,11 +16,11 @@ class CsDepot(Depot):
         for product in products:
             productAmount = (
                 self.subscription_set.filter(
-                    q_isactive(), content__products__product__id=product.id
+                    q_isactive(), custom__products__product__id=product.id
                 )
                 .order_by("primary_member__first_name", "primary_member__last_name")
-                .aggregate(Sum("content__products__amount"))[
-                    "content__products__amount__sum"
+                .aggregate(Sum("custom__products__amount"))[
+                    "custom__products__amount__sum"
                 ]
                 or 0
             )
